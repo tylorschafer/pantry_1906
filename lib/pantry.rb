bfab8708acab062ad7c38331130cffd174a1ba5d
Tylor Schafer <tylorschafer@gmail.com>
2019-07-30 09:40:05 -0600

    Work up to enough_ingredients_for

---
+++ lib/pantry.rb
@@ -0,0 +1,21 @@
+class Pantry
+  attr_reader :stock
+
+  def initialize
+    @stock = Hash.new(0)
+  end
+
+  def stock_check(ingredient)
+    @stock[ingredient]
+  end
+
+  def restock(ingredient, amount)
+    @stock[ingredient] += amount
+  end
+
+  def enough_ingredients_for?(recipe)
+    @stock.all? do |ingredient, quantity|
+      recipe.amount_required(ingredient) <= quantity
+    end
+  end
+end
